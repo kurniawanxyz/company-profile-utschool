a5b2f38224ceb6360bd2764c5f1ac12697abd571
kurniawanxyz <kaitodecode@gmail.com>
2024-09-03 10:35:24 +0700

    feat: paginate gallery and delte gallery

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b79dffc57362f908a5f6a451c4c4bc1543a7f43b596300691f754f5c8b320312
-size 263
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});

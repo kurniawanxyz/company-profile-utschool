50a82bd3ed9342733fd010185f304ef6556d7484
Verdi <104150927+gioVerdiansyah@users.noreply.github.com>
2024-09-03 13:23:17 +0700

    feat: gallery categories seeder

---
+++ backend/database/factories/GalleryCategoryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\GalleryCategory>
+ */
+class GalleryCategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'text' => fake('id')->unique()->words(2, true),
+            'description' => fake('id')->unique()->sentence(),
+        ];
+    }
+}

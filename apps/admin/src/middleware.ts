54c3b054a6a047955f2f89377436aa56128688c1
kurniawanxyz <kaitodecode@gmail.com>
2024-09-11 08:39:00 +0700

    change directory from /frontend to /admin

---
+++ apps/admin/src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+
+  console.log(request.nextUrl.pathname);
+
+  if (request.nextUrl.pathname == "/") {
+    return NextResponse.rewrite(new URL("/admin/login", request.url));
+  }
+
+  // Jika token tidak ada, redirect ke halaman login
+  if (!token) {
+    return NextResponse.rewrite(new URL("/admin/login", request.url));
+  }
+
+  if (request.nextUrl.pathname == "/admin/login") {
+    return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// Configurasi route yang ingin dikenakan middleware
+export const config = {
+  matcher: ["/admin/:path*", "/"], // Atur path yang perlu dicek
+};

0976f1c9458f886daef23b5147f649b33e2d60ac
kurniawanxyz <kaitodecode@gmail.com>
2024-09-17 13:24:08 +0700

    feat: edit batch

--- apps/admin/src/services/BatchService.tsx
+++ apps/admin/src/services/BatchService.tsx
@@ -12,3 +12,18 @@ export async function handleCreate(fd: FormData) {
       redirect("/admin/batch")
     }
 }
+
+export async function handleUpdate(fd: FormData) {
+    const id = fd.get("id")
+    const option: RequestInit = {
+      method: "PUT",
+      body: JSON.stringify({
+        number: fd.get("number"),
+        training_program_id: fd.get("training_program_id")
+      })
+    }
+    const [status,,] = await handleFetch('/admin/batch/'+id, option, false, true)
+    if (status) {
+      redirect("/admin/batch")
+    }
+}

838ad7a93f147cab91e1b88f996eae5eb73b1405
kurniawanxyz <kaitodecode@gmail.com>
2024-09-17 08:50:17 +0700

    wip: paginate

--- apps/admin/src/services/LearningPointService.ts
+++ apps/admin/src/services/LearningPointService.ts
@@ -6,7 +6,7 @@ export async function handleCreate(fd: FormData) {
     method: "POST",
     body: fd
   }
-  const [status,,] = await handleFetch('/admin/learning_point', option, true, true)
+  const [status,,] = await handleFetch('/admin/learning-point', option, true, true)
   if (status) {
     redirect("/admin/learning-point")
   }

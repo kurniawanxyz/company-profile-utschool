8f77d64fc7b2bfdecb9cffb83efb4cf47e1a6ec5
kurniawanxyz <kaitodecode@gmail.com>
2024-10-15 13:25:11 +0700

    lint

--- apps/admin/src/app/error.tsx
+++ apps/admin/src/app/error.tsx
@@ -5,7 +5,7 @@ const ErrorComponent: React.FC = () => {
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>Something went wrong</h1>
-            <p>We're sorry, but an unexpected error has occurred.</p>
+            <p>Were sorry, but an unexpected error has occurred.</p>
         </div>
     );
 };

0c7fcf62f7ab626e7eca64e553e34d7cf9f5ed17
kurniawanxyz <kaitodecode@gmail.com>
2024-10-24 15:58:47 +0700

    update academic card

--- apps/company-profile/src/components/molecules/academic-card.molecules.tsx
+++ apps/company-profile/src/components/molecules/academic-card.molecules.tsx
@@ -7,7 +7,7 @@ type Props = {}
 export default function AcademicCard({}: Props) {
   return (
     <aside className=''>
-        <div className="grid grid-cols-3 mt-8 justify-items-center">
+        <div className="grid grid-cols-3 mt-8 w-full gap-24 ">
             <Card className='relative h-[400px] w-[300px] rounded-2xl'>
                 <Img src='/images/students/1.jpg' className='absolute w-full h-full object-cover object-center top-0 left-0 right-0 bottom-0'/>
                 <Button className='absolute bottom-10 left-1/2 -translate-x-1/2 text-xl px-10 py-2 rounded-full' variant="outline">Mechanic</Button>
